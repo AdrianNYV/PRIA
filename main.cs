64129f66259636ca4a672603aa02f75837092432
David Corbelle García <corgardav@IESPIRINGALLA.AD>
2023-01-17 20:44:46 +0100

    ZAWARDO

--- main.cs
+++ main.cs
@@ -1,47 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using System;
+using System.IO;
+using System.Text;
 
-public abstract class Unit
-{
-    private int life=20;
-    private int atack=0;
+public class Programa{
 
-    public Unit (int atack){
-        this.atack=atack;
+    public static void Main(){
+        Console.WriteLine("ZAWARDO");
+    }
+    public abstract class Unit
+    {
+        private int life=20;
+        private int atack=0;
+
+        public Unit (int atack){
+            this.atack=atack;
+        }
+
+        public void Hit(Unit defensor){
+            int newLife= defensor.getVida()- this.getAtaque();
+            defensor.setVida(newLife);
+        }
+
+        public int getAtaque(){
+            return atack;
+        }
+        public int getVida(){
+            return life;
+        }
+        public void setVida(int newLife){
+            this.life=newLife;
+        }
+
     }
 
-    public void Hit(Unit defensor){
-        int newLife= defensor.getVida()- this.getAtaque();
-        defensor.setVida(newLife);
+    public class Aldeano : Unit
+    {
+        public Aldeano(): base(0){}
+        
     }
 
-    public int getAtaque(){
-        return atack;
-    }
-    public int getVida(){
-        return life;
-    }
-    public void setVida(int newLife){
-        this.life=newLife;
+    public class Guerrero: Unit
+    {
+        public Guerrero(): base(10){}
+        
     }
 
-}
-
-public class Aldeano : Unit
-{
-    public Aldeano(): base(0){}
-    
-}
-
-public class Guerrero: Unit
-{
-    public Guerrero(): base(10){}
-    
-}
-
-public class Arquero: Unit
-{
-    public Arquero(): base(5){}
-    
+    public class Arquero: Unit
+    {
+        public Arquero(): base(5){}
+        
+    }
 }
